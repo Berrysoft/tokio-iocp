043b7a1f073d90ab1667f09c12fb02820d9499ed
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-09-07 23:58:28 +0800

    Add mod docs.

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,3 +1,13 @@
+//! TCP/UDP bindings for IOCP.
+//!
+//! This module contains the TCP/UDP networking types, similar to the standard
+//! library, which can be used to implement networking protocols.
+//!
+//! # Organization
+//!
+//! * [`TcpListener`] and [`TcpStream`] provide functionality for communication over TCP
+//! * [`UdpSocket`] provides functionality for communication over UDP
+
 mod socket;
 mod tcp;
 mod udp;
