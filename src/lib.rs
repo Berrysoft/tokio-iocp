96b583da29307e66cc936cbabe286be00d0b0863
Berrysoft <Strawberry_Str@hotmail.com>
2022-09-07 17:42:09 +0800

    Add more docs.

--- src/lib.rs
+++ src/lib.rs
@@ -50,15 +50,32 @@ pub mod fs;
 mod io_port;
 pub mod net;
 mod op;
-pub mod runtime;
+mod runtime;
 
 #[doc(no_inline)]
 pub use runtime::spawn;
 #[doc(no_inline)]
 pub use std::io::{Error as IoError, Result as IoResult};
 
+/// Start an IOCP enabled Tokio runtime.
+///
+/// All `tokio-iocp` resource types must be used from within the context of a
+/// runtime. The `start` method initializes the runtime and runs it for the
+/// duration of `future`.
+///
+/// The `tokio-iocp` runtime is compatible with all Tokio, so it is possible to
+/// run Tokio based libraries (e.g. hyper) from within the tokio-iocp runtime.
+/// A `tokio-iocp` runtime consists of a Tokio `current_thread` runtime.
+/// All tasks spawned on the `tokio-iocp` runtime are executed on the current thread.
+/// To add concurrency, spawn multiple threads, each with a `tokio-iocp` runtime.
 pub fn start<F: std::future::Future>(future: F) -> F::Output {
     runtime::Runtime::new().unwrap().block_on(future)
 }
 
+/// A specialized `Result` type for IOCP operations with buffers.
+///
+/// This type is used as a return value for asynchronous IOCP methods that
+/// require passing ownership of a buffer to the runtime. When the operation
+/// completes, the buffer is returned whether or not the operation completed
+/// successfully.
 pub type BufResult<T, B> = (IoResult<T>, B);
